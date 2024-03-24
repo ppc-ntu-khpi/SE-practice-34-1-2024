efc09440bcada28e8447fd25105772b9f90fa9eb
Roma-Cherny-15 <145929935+Roma-Cherny-15@users.noreply.github.com>
2024-03-24 19:42:48 +0200

    source files

---
+++ src/Assets/Plugins/OpenLinks.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class OpenLinks : MonoBehaviour
+{
+    public static void OpenURL(string url)
+    {
+        #if !UNITY_EDITOR && UNITY_WEBGL
+        OpenTab(url);
+        #endif
+    }
+
+    [DllImport("__Internal")]
+    private static extern void OpenTab(string url);
+}
